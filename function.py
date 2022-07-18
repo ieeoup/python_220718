334ba47f5de85e59740e9988dc4e512148c2a745
ieeoup <ieeoup@naver.com>
2022-07-18 16:39:58 +0900

    first

---
+++ function.py
@@ -0,0 +1,36 @@
+#function1.py
+
+# 함수정의
+import re
+from winreg import SetValue
+
+
+def times(a,b):
+    return a+b, a*b
+
+#함수호출
+result = times(3,4)
+print(result)
+
+#함수정의
+def setValue(newValue):
+    x = newValue
+    print("함수 내부:", x)
+
+#호출
+result = setValue(5)
+print(result)
+
+#교집합을 리턴하는 함수
+def intersect(prelist, postlist):
+    #지역변수로 리스트를 초기화
+    result = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        #어떤 글자가 postlist에 포함되어 있고 x가 result에 없으면
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
+
+#호출
+print(intersect("HAM", "SPAM"))
