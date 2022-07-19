eaca49c765d4688555828738773db0e0e66ae60e
ieeoup <ieeoup@naver.com>
2022-07-19 11:06:47 +0900

    first commit

---
+++ ifelse.py
@@ -0,0 +1,22 @@
+# ifelse.py
+
+# 다중라인으로 주석 처리 : ctrl + / (cmd + /)
+# score = int(input("점수를 입력:"))70
+
+# if 90 <= score <= 100:
+#     grade = "A"
+# elif 80 <= score < 90:
+#     grade = "B"
+# elif 70 <= score < 80:
+#     grade = "C"
+# else:
+#     grade = "D"
+
+# print("등급은 :", grade)
+
+#반복문
+value = 5
+while value > 0:
+    print(value)
+    value -= 1
+
